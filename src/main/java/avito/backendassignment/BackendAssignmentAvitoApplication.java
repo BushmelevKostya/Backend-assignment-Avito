3fbc16d4478324e0f0915f08d8fb75d73fa1b058
Kostya <kostikbushmelev@gmail.com>
2025-04-20 22:48:37 +0300

    init project

---
+++ src/main/java/avito/backendassignment/BackendAssignmentAvitoApplication.java
@@ -0,0 +1,13 @@
+package avito.backendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignmentAvitoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignmentAvitoApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/avito/backendassignment/BackendAssignmentAvitoApplicationTests.java
@@ -0,0 +1,13 @@
+package avito.backendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssignmentAvitoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
